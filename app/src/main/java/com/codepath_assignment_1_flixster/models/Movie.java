668a5bc1c2042dacd0f280106f1a2a5135262379
Cherie Young <44423254+youncher@users.noreply.github.com>
2019-10-18 02:04:19 -0700

    Added API call to get movie list and added Movie object.

---
+++ app/src/main/java/com/codepath_assignment_1_flixster/models/Movie.java
@@ -0,0 +1,50 @@
+package com.codepath_assignment_1_flixster.models;
+
+import android.util.Log;
+
+import com.codepath.asynchttpclient.AsyncHttpClient;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Headers;
+
+public class Movie {
+
+    private String posterPath;
+    private String title;
+    private String overview;
+
+    public Movie(JSONObject jsonObject) throws JSONException {
+        posterPath = jsonObject.getString("poster_path");
+        title = jsonObject.getString("title");
+        overview = jsonObject.getString("overview");
+    }
+
+    public static List<Movie> fromJSONArray(JSONArray movieJsonArray) throws JSONException {
+        List<Movie> movies = new ArrayList<>();
+
+        for (int i = 0; i < movieJsonArray.length(); i++) {
+            movies.add(new Movie(movieJsonArray.getJSONObject(i)));
+        }
+
+        return movies;
+    }
+
+    public String getPosterPath() {
+        return String.format("https://image.tmdb.org/t/p/w342%s", posterPath);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+}
